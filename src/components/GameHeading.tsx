183af3d18b7a47faf84f9e765129e3553500c56a
Emilola Divine <emiloladivine123@gmail.com>
2024-07-09 19:43:17 +0100

    Refactor - create look ups hooks

--- src/components/GameHeading.tsx
+++ src/components/GameHeading.tsx
@@ -3,17 +3,16 @@ import { GameQuery } from "../App";
 import useGenres from "../hooks/useGenres";
 import usePlatforms from "../hooks/usePlatforms";
 import platforms from "../data/platforms";
+import usePlatform from "../hooks/usePlatform";
+import useGenre from "../hooks/useGenre";
 
 interface Props {
    gameQuery: GameQuery;
 }
 
 const GameHeading = ({ gameQuery }: Props) => {
-   const { data: genres } = useGenres();
-   const genre = genres?.results.find(genre => genre.id === gameQuery.genreId);
-
-   const { data: platforms } = usePlatforms();
-   const platform = platforms?.results.find(platform => platform.id === gameQuery.platformId);
+   const genre = useGenre(gameQuery.genreId);
+   const platform = usePlatform(gameQuery.platformId);
 
    const heading = `${platform?.name || ''} ${genre?.name || ''} Games`;
 
--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,7 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
-import APIClient from "../services/api-client";
-import { FetchResponse } from "../services/api-client";
 import genres from "../data/genres";
+import APIClient from "../services/api-client";
 export interface Genre {
    id: number;
    name: string;
@@ -15,7 +14,7 @@ const useGenres = () =>
       queryKey: ["genres"],
       queryFn: apiClient.getAll,
       staleTime: 24 * 60 * 60 * 1000, //24hr
-      initialData: { count: genres.length, results: genres },
+      initialData: genres,
    });
    
 export default useGenres;
