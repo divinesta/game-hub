cc2a15b459914d82c4141e902df5cd59f7caf394
Emilola Divine <emiloladivine123@gmail.com>
2024-07-09 10:32:22 +0100

    Implement Infinite scroll

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -29,7 +29,8 @@ const useGames = (gameQuery: GameQuery) =>
          }),
          getNextPageParam: (lastPage, allpages) => {
             return lastPage.next ? allpages.length + 1 : undefined;
-         }
+         },
+         staleTime: 24 * 60 * 60 * 1000 //24hrs
    });
 
 
