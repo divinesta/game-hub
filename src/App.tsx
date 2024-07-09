6239732d249ecf93f01dea4ab10648984ddcce70
Emilola Divine <emiloladivine123@gmail.com>
2024-07-09 10:54:32 +0100

    Refactor: Simplify game query

--- src/App.tsx
+++ src/App.tsx
@@ -9,9 +9,12 @@ import PlatformSelector from "./components/PlatformSelector";
 import SortSelector from "./components/SortSelector";
 import GameHeading from "./components/GameHeading";
 
+// undefined: the absence of a  value
+// null: the intentional absence of a value
+
 export interface GameQuery {
-   genre: Genre | null;
-   platform: Platform | null;
+   genreId?: number;
+   platformId?: number;
    sortOrder: string;
    searchText: string;
 }
@@ -40,9 +43,9 @@ function App() {
          <Show above="lg">
             <GridItem area="aside" paddingX={5}>
                <GenreList
-                  selectedGenre={gameQuery.genre}
+                  selectedGenreId={gameQuery.genreId}
                   onSelectGenre={(genre) =>
-                     setGameQuery({ ...gameQuery, genre })
+                     setGameQuery({ ...gameQuery, genreId: genre.id })
                   }
                />
             </GridItem>
@@ -53,9 +56,9 @@ function App() {
                <Flex marginBottom={5}>
                   <Box marginRight={5}>
                      <PlatformSelector
-                        selectedPlatform={gameQuery.platform}
+                        selectedPlatformId={gameQuery.platformId}
                         onSelectPlatform={(platform) =>
-                           setGameQuery({ ...gameQuery, platform })
+                           setGameQuery({ ...gameQuery, platformId: platform.id })
                         }
                      />
                   </Box>
