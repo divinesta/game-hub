68925d5ebc219a44ae2bf27f9a6b5fb7894b2b9d
Emilola Divine <emiloladivine123@gmail.com>
2024-07-26 03:44:22 +0100

    Improve the layout

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -1,4 +1,4 @@
-import { Heading, Spinner } from "@chakra-ui/react";
+import { Heading, GridItem, Spinner, SimpleGrid } from "@chakra-ui/react";
 import { useParams } from "react-router-dom";
 import ExpandableText from "../components/ExpandableText";
 import GameAttributes from "../components/GameAttributes";
@@ -15,13 +15,18 @@ const GameDetailPage = () => {
   if (error) throw error;
 
   return (
-    <>
-      <Heading>{game?.name}</Heading>
-      <ExpandableText children={game?.description_raw}></ExpandableText>
-      <GameAttributes game={game}/>
-      <GameTrailer gameId={game!.id}/>
-      <GameScreenshots gameId={game!.id}/>
-    </>
+    <SimpleGrid columns={{ base:1, md: 2 }} spacing={5}>
+      <GridItem>
+        <Heading>{game?.name}</Heading>
+        <ExpandableText children={game?.description_raw}></ExpandableText>
+        <GameAttributes game={game}/>
+      </GridItem>
+      
+      <GridItem>
+        <GameTrailer gameId={game!.id}/>
+        <GameScreenshots gameId={game!.id}/>
+      </GridItem>
+    </SimpleGrid>
   );
 };
 
